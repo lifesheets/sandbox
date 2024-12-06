0b651dcb794870429aee60a679f9a9ca22713422
ua.lifesheets <ua.lifesheets@gmail.com>
2024-12-06 05:20:03 +0200

    Get a list of all PHP files in the functions folder, excluding 'requires'

--- system/autoload.php
+++ system/autoload.php
@@ -10,3 +10,21 @@ if (!file_exists(LCMS . '/bootstrap.php')) {
     exit('Системні файли не знайдено.');
 }
 
+# Функція для рекурсивного пошуку PHP-файлів
+require_once LCMS . '/functions/requires/RecursiveFileFinder.php';                      
+
+# Отримання списку всіх PHP-файлів у папці functions, виключаючи 'requires'
+$function_files = array_filter(
+    // Рекурсивний пошук файлів
+    getPhpFilesFromDirectory(LCMS . '/functions'),
+    function ($path) {
+        // Фільтрація шляхів
+        return strpos($path, 'system/functions/requires') === false;
+    }
+);
+
+# Завантаження всіх функцій
+foreach ($function_files as $function_file) {
+    require_once($function_file);
+}
+
