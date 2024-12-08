9029aa100f57f0154eaed0f510b7cc189def3738
ua.lifesheets <ua.lifesheets@gmail.com>
2024-12-09 00:27:04 +0200

    Setting HTTP headers

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
 
+# Встановлення заголовків HTTP
+header('Content-Type: text/html; charset=UTF-8');                                       // Встановлення кодування UTF-8
+header('Powered: LiveCMS');                                                             // Ідентифікатор CMS у заголовку
+header('Cache-control: public');                                                        // Публічне кешування
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 60 * 60 * 24) . ' GMT');         // Термін дії кешу — 1 день
+
 // Визначення кореневого шляху проєкту
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
-define('LCMS', ROOT . '/System');
+define('LCMS', ROOT . '/system');
 
 // Підключення автозавантажувача
-require_once LCMS . '/Classes/Autoloader.php';
+require_once LCMS . '/Autoloader.php';
 
 // Підключення обробника помилок
 require_once LCMS . '/Classes/ErrorHandler.php';
 
 // Реєстрація автозавантажувача та ініціалізація обробника помилок
-use System\Classes\Autoloader;
+use System\Autoloader;
 use System\Classes\ErrorHandler;
 
 Autoloader::register();
